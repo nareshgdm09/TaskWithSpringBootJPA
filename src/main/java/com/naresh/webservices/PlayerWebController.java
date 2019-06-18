2b1793579d3f759322ad4fc9186dbbeebe73f3af
Naresh <nareshg4020@gmail.com>
2019-06-18 08:44:18 +0530

    added extra mapping

--- src/main/java/com/naresh/webservices/PlayerWebController.java
+++ src/main/java/com/naresh/webservices/PlayerWebController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.naresh.entity.Player;
 import com.naresh.service.PlayerWebService;
 
 @RestController
+@RequestMapping("/api")
 public class PlayerWebController {
 	@Autowired
 	PlayerWebService playerWebService;
